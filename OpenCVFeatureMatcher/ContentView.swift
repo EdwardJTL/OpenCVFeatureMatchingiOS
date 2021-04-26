39594dbde3b392bdfb11a52726d1677d181e8b42
Edward Luo <edward.luo@mail.utoronto.ca>
2021-04-26 16:23:09 -0400

    Switch to SwiftUI

---
+++ OpenCVFeatureMatcher/ContentView.swift
@@ -0,0 +1,15 @@
+//
+//  ContentView.swift
+//  OpenCVFeatureMatcher
+//
+//  Created by Edward Luo on 2021-04-26.
+//
+
+import Foundation
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello!")
+    }
+}
