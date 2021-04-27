b37049ed78c2e68586d7b3392066fb8845469e13
Edward Luo <edward.luo@mail.utoronto.ca>
2021-04-26 21:43:31 -0400

    Image Picker

--- OpenCVFeatureMatcher/ContentView.swift
+++ OpenCVFeatureMatcher/ContentView.swift
@@ -9,22 +9,37 @@ import Foundation
 import SwiftUI
 
 struct ContentView: View {
-    @State var image = Image("cat")
+    @State private var showPhotoLibrary = false
+    @State var image = UIImage(named: "cat")!
 
     var body: some View {
         VStack {
-            image
+            Image(uiImage: image)
                 .resizable()
                 .scaledToFit()
             Button("Change to gray") {
-                let grayImage = OpenCVWrapper.toGray(UIImage(named: "cat")!)
-                image = Image(uiImage: grayImage)
+                image = OpenCVWrapper.toGray(image)
             }
             Button("Change to keypoint") {
-                let kptImage = OpenCVWrapper.toKeypointImage(UIImage(named: "cat")!)
-                image = Image(uiImage: kptImage)
+                image = OpenCVWrapper.toKeypointImage(image)
             }
+            Button(action: {
+                self.showPhotoLibrary.toggle()
+            }, label: {
+                HStack {
+                    Image(systemName: "photo")
+                        .font(.system(size: 20))
+                    Text("PhotoLibrary")
+                        .font(.headline)
+                }
+                .foregroundColor(.white)
+                .padding()
+                .background(Capsule().foregroundColor(.blue))
+            })
         }
         .padding()
+        .sheet(isPresented: $showPhotoLibrary) {
+            ImagePicker(sourceType: .photoLibrary, selectedImage: $image)
+        }
     }
 }
